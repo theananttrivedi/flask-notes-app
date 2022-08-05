af9d30f8b84c121c68e47cdbe2417dc88126a555
ananttrivedi0709 <theananttrivedi@gmail.com>
2022-08-05 13:45:11 +0530

    PREPARED HOME PAGE

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -8,26 +8,59 @@ const HomePage = () => {
   }, []);
   return (
     <>
-      <div style={styles.container}>HomePage</div>
+      <div style={styles.container}>
+        <img style={styles.coverimg} src={"/book.webp"} />
+        <div style={styles.ad}>
+          {" "}
+          <h2>
+            Create a <span style={styles.blue}>Group</span>
+          </h2>
+          <h2>
+            Add <span style={styles.blue}>Notes</span>
+          </h2>
+          <h2>
+            Start <span style={styles.blue}>Learning</span>
+          </h2>
+        </div>
+        <img style={styles.coverimg2} src={"/house.png"} />
+      </div>
     </>
   );
 };
 
 const styles = {
-  homeButton: {
-    position: "fixed",
-    top: "5vh",
-    right: "0rem",
-    // marginRight: "1rem",
-    zIndex: "8000",
-    backgroundColor: "blue",
-  },
   container: {
     height: "90vh",
     position: "fixed",
-    backgroundColor: "lightcoral",
+    background: `linear-gradient(
+      to bottom right,
+      rgba(21, 21, 21, 1),
+      rgba(21, 21, 21, 0.8)
+    )`,
     top: "5vh",
     width: "100vw",
+    overflow: "hidden",
+  },
+  ad: {
+    color: "white",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    zIndex: 2000,
+    backdropFilter: "blur(1rem)",
+  },
+  blue: {
+    color: "rgb(6, 123, 192)",
+  },
+  coverimg: {
+    width: "80vw",
+  },
+  coverimg2: {
+    width: "80vw",
+    position: "fixed",
+    bottom: 0,
+    left: "0",
   },
 };
 
